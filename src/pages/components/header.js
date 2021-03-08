c86f9c68c97d9283f28b617d3fa9dbf636116439
Omid <>
2021-03-08 13:22:16 +0100

    added .travis.yml

---
+++ src/pages/components/header.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Header = () => (
+  <header>
+    <div>
+      <h1> Hello Fanny! Greetings from north of the town.</h1>
+    </div>
+  </header>
+)
+
+export default Header
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,12 @@
 import React from "react"
+import Header from "./components/header"
 
 export default function Home() {
-  return <div>Let's practise some CI</div>
+  return (
+    <div>
+      Let's practise some CI
+      <br />
+      <Header />
+    </div>
+  )
 }
